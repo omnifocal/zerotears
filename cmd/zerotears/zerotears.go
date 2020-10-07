8d3f572bc904987561bd9553d214ad9065c33ab9
Jack Turner <jack.turner@asteriskinfosec.com.au>
2020-10-07 14:25:44 +0800

    basic args parsing

--- cmd/zerotears/zerotears.go
+++ cmd/zerotears/zerotears.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/omnifocal/zerotears/pkg/zerotears"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
@@ -11,12 +14,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	c := zerotears.Init(host, string(dat))
 
-	info := c.GetStatus()
-	zerotears.PrintStatus(&info)
+	// getStatusCmd := flag.NewFlagSet("status", flag.ExitOnError)
+	// getInfoCmd := flag.NewFlagSet("info", flag.ExitOnError)
+	// listNetsCmd := flag.NewFlagSet("ls-networks", flag.ExitOnError)
+	createNetCmd := flag.NewFlagSet("mk-network", flag.ExitOnError)
+	createNetName := createNetCmd.String("name", "", "name")
 
-	networks := c.ListNetworks()
-	zerotears.PrintNetworkIDs(networks)
+	switch os.Args[1] {
+	case "status":
+		status := c.GetStatus()
+		zerotears.PrintStatus(&status)
+	case "info":
+		info := c.GetControllerInfo()
+		zerotears.PrintControllerInfo(&info)
+	case "ls-networks":
+		networks := c.ListNetworks()
+		zerotears.PrintNetworkIDs(networks)
+	case "mk-network":
+		createNetCmd.Parse(os.Args[2:])
+		newNet := c.CreateNetwork(*createNetName)
+		fmt.Println(newNet)
+	default:
+		fmt.Println("No valid subcommand was provided")
+		os.Exit(1)
+	}
 }
